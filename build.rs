ea06f21eae131ec187646a173c74d0fb999e01a8
Floris Cornel <floris@cornel.email>
2022-12-10 21:35:21 +0000

    refactor build.rs

--- build.rs
+++ build.rs
@@ -10,46 +10,13 @@ const FREQUENCIES_CSV_PATH: &str = "./data/5-letter_frequencies.csv";
 
 type Word = [u8; WORDLE_LETTER_COUNT];
 
-fn get_word_list() -> Vec<Word> {
-    let mut rdr = csv::Reader::from_path(WORDS_TXT_PATH).unwrap();
-    let mut list = vec![];
-
-    for result in rdr.records() {
-        let record = result.unwrap();
-        let word = record.get(0).unwrap();
-        if word.len() == WORDLE_LETTER_COUNT {
-            list.push(word.to_uppercase().as_bytes().try_into().unwrap());
-        }
-    }
-    list
-}
-
-// function that reads a csv file from a path and returns a vector of Frequency
-fn get_frequency_map() -> HashMap<Word, u64> {
-    let mut rdr = csv::Reader::from_path(FREQUENCIES_CSV_PATH).unwrap();
-    let mut frequencies = HashMap::new();
-
-    for result in rdr.records() {
-        let record = result.unwrap();
-        let word = record.get(0).unwrap();
-        let count = record.get(1).unwrap().parse::<u64>().unwrap();
-        if word.len() == WORDLE_LETTER_COUNT {
-            frequencies.insert(word.to_uppercase().as_bytes().try_into().unwrap(), count);
-        }
-    }
-    frequencies
-}
-
 fn main() {
     let path = Path::new(&env::var("OUT_DIR").unwrap()).join("codegen.rs");
     let mut file = BufWriter::new(File::create(&path).unwrap());
-
     let mut map = phf_codegen::Map::new();
 
-    let words = get_word_list();
     let frequencies = get_frequency_map();
-
-    for word in words {
+    for word in get_word_list() {
         let count = frequencies.get(&word).unwrap_or(&0);
         map.entry(word, &format!("{count}"));
     }
@@ -68,4 +35,33 @@ fn main() {
     )
     .unwrap();
     writeln!(&mut file, ";").unwrap();
+}
+
+fn get_word_list() -> Vec<Word> {
+    let mut rdr = csv::Reader::from_path(WORDS_TXT_PATH).unwrap();
+    let mut list = vec![];
+
+    for result in rdr.records() {
+        let record = result.unwrap();
+        let word = record.get(0).unwrap();
+        if word.len() == WORDLE_LETTER_COUNT {
+            list.push(word.to_uppercase().as_bytes().try_into().unwrap());
+        }
+    }
+    list
+}
+
+fn get_frequency_map() -> HashMap<Word, u64> {
+    let mut rdr = csv::Reader::from_path(FREQUENCIES_CSV_PATH).unwrap();
+    let mut frequencies = HashMap::new();
+
+    for result in rdr.records() {
+        let record = result.unwrap();
+        let word = record.get(0).unwrap();
+        let count = record.get(1).unwrap().parse::<u64>().unwrap();
+        if word.len() == WORDLE_LETTER_COUNT {
+            frequencies.insert(word.to_uppercase().as_bytes().try_into().unwrap(), count);
+        }
+    }
+    frequencies
 }
